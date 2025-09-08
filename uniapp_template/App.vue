87b94b1647c2353538fcbc49a6f46a3f2a2a9203
Null <779217162@qq.com>
2025-09-08 11:43:00 +0800

    feat: 提交uniapp_template

--- uniapp_template/App.vue
+++ uniapp_template/App.vue
@@ -1,5 +1,10 @@
 <script>
 	import CONFIG from '@/common/config/index.js'
+	import {
+		heartBeatInstance
+	} from '@/common/heartBeat/index.js'
+
+
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
@@ -10,12 +15,22 @@
 				"background:#007aff ;padding: 1px; color: #fff; font-weight: bold;",
 				`background: ${ env === 'development' ? '#dd524d' : '#4cd964' };padding: 1px 6px 1px 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;`
 			);
+			this.registerHeartBeatEvent()
 		},
 		onShow: function() {
 			console.log('App Show')
+			heartBeatInstance?.reStart()
 		},
 		onHide: function() {
 			console.log('App Hide')
+			heartBeatInstance?.stop()
+		},
+		methods: {
+			registerHeartBeatEvent() {
+				heartBeatInstance.on('change', (e) => {
+					console.log('请求接口=====>心跳')
+				})
+			}
 		}
 	}
 </script>
